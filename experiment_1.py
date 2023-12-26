Fixed problem with numerical integration; need to have fixed step size for euler integration which requires properly rounding various quantitities.

experiment_1.py
@@ -4,28 +4,39 @@ import numpy as np
 from symmetries import *
 from stochastic_systems import *
 from sklearn.feature_selection import mutual_info_regression
+import matplotlib.pyplot as plt
 import pdb
 
 def main():
     r = np.array([1.])
-    k = np.array([100.])
+    k = np.array([1000.])
     alpha = ([[1.]])
-    sigma = np.array([0.])
+    sigma = np.array([0.1])
     gamma_A = 1.
     params_A = [r, k, alpha, sigma, gamma_A]
     gamma_B =  1.2
     params_B = [r, k, alpha, sigma, gamma_B]
     t_max = 2.
+    num_times = 10
 
 #    init_x = []
 #    for ii in range(10):
 #        init_x.append(np.array(np.abs(np.random.normal(5., 0.5)), ndmin=1))
     init_x = np.array([5.])
-    data_A = random_time_intervention(init_x, params_A, t_max, lvsym)
-    data_B = random_time_intervention(init_x, params_B, t_max, lvsym)
+    data_A = np.round(random_time_intervention(init_x, params_A, t_max, lvsym,
+                                      num_times=num_times), 2)
+    data_B = np.round(random_time_intervention(init_x, params_B, t_max, lvsym,
+                                      num_times=num_times), 2)
+    print("Data A")
+    print(data_A)
+    print("Data B")
+    print(data_B)
     mi_A = mutual_info_regression(data_A[:,0].reshape(-1,1), data_A[:,1])
     mi_B = mutual_info_regression(data_B[:,0].reshape(-1,1), data_B[:,1])
     print("MI logistic: {} MI generalized logistic: {}".format(mi_A, mi_B))
+    plt.plot(data_A[:,0], data_A[:,1], 'ob')
+    plt.plot(data_B[:,0], data_B[:,1], 'or')
+    plt.show()
 
 if __name__ == "__main__":
     main()

stochastic_systems.py
@@ -17,7 +17,8 @@ class LotkaVolterraSND( object ):
         Systems - Liu, M. & Fan M.; J Nonlinear Sci (2017)
     """
 
-    def __init__(self, r, k, alpha, sigma, init_x, gamma=1., init_t=0, steps=10**3):
+    def __init__(self, r, k, alpha, sigma, init_x, gamma=1., init_t=0,
+                 steps=10**3, fixed_step=False):
         """ Initializes a stochastic competitive Lotka-Volterra model with n 
             species
         
@@ -50,9 +51,8 @@ class LotkaVolterraSND( object ):
 
         self._x = copy.copy(init_x)
         self._time = float(init_t)
-        self._delta_t = 1
         self._steps = steps
-        
+        self._fixed_step = fixed_step
         self._sigma = sigma
 
 
@@ -60,10 +60,16 @@ class LotkaVolterraSND( object ):
 
         if elapsed_time == 0.:
             return None
-
-        delta = float(elapsed_time) / float(self._steps)
-        if delta < 0:
-            print(delta)
+        
+        remainder = None
+        if self._fixed_step:
+            delta = float(10 ** (-3))
+            self._steps = int(round(elapsed_time / delta))
+            if not elapsed_time - self._steps * delta <= 10.**(-9):
+                pdb.set_trace()
+            
+        else:
+            delta = float(elapsed_time) / float(self._steps)
         X = self._x
         dX = np.zeros(len(X))
         for s in range(self._steps):
@@ -76,6 +82,16 @@ class LotkaVolterraSND( object ):
                 * (noise**2 - 1.))
 
                 X[i] = X[i] + dX[i] * delta
+                X[i] = np.max([X[i], 0.])
+            if remainder is not None:
+                noise = np.random.normal()
+                
+                dX[i] = self._r[i] * (X[i] ** self._gamma) * (1. - (np.sum(self._alpha[i] *
+                X)/self._k[i]) ) + (self._sigma[i] * X[i] * noise / (
+                np.sqrt(remainder) ) ) + (self._sigma[i]**2 / 2.) * (X[i]
+                * (noise**2 - 1.))
+
+                X[i] = X[i] + dX[i] * remainder
                 X[i] = np.max([X[i], 0.])
  
         self._x = X
@@ -99,12 +115,14 @@ def point_sample(
         params, 
         times=np.linspace(0., 10., 100), 
         duration=10, 
-        num_samples=10
+        num_samples=10,
+        fixed_step=False
         ):
     
     r, k, alpha, sigma, gamma = params
 
-    LVSND = LotkaVolterraSND(r, k, alpha, sigma, init_x[0], gamma=gamma)
+    LVSND = LotkaVolterraSND(r, k, alpha, sigma, init_x[0], gamma=gamma,
+                             fixed_step=fixed_step)
 
     data = []
     for ii in range(len(init_x)):
@@ -124,7 +142,8 @@ def random_time_intervention(
 
     r, k, alpha, sigma, gamma = params
 
-    LVSND = LotkaVolterraSND(r, k, alpha, sigma, init_x, gamma=gamma)
+    LVSND = LotkaVolterraSND(r, k, alpha, sigma, init_x, gamma=gamma,
+                             fixed_step=True)
 
 #    times = np.sort(np.random.rand(num_times) * t_max)
     
@@ -135,16 +154,16 @@ def random_time_intervention(
         LVSND._x = copy.copy(init_x)
 
         # choose a random time
-        tt = np.random.rand() * t_max
+        tt = np.round(np.random.rand() * t_max, 3)
 
         # evolve to tt
-        LVSND.update_x(tt - ts)
-
+        LVSND.update_x(np.round(tt - ts, 3))
+    
         # apply func to change state
         LVSND._x = func(copy.copy(LVSND._x), r, k)
 
         # evolve to tmax
-        LVSND.update_x(t_max - tt)
+        LVSND.update_x(np.round(t_max - tt, 3))
 
         # read state and store with tt
         data.append(np.concatenate([np.array(tt, ndmin=2),

test_stochastic_systems.py
@@ -93,6 +93,23 @@ def test_point_sample():
         plt.plot(times, ss, 'k.')
 
 
+    # TEST 8
+    r = np.array([1., 1.])
+    k = np.array([100., 100.])
+    alpha = ([[1., -0.2], [-0.2, 1.]])
+    sigma = np.array([0.1, 0.1])
+    gamma = 1.
+    init_x = [np.array([5., 5.]), np.array([40., 40.])]
+    times = np.linspace(0., 6., 50)
+    samples8 = point_sample(init_x, [r, k, alpha, sigma, gamma], times,
+                            fixed_step=True)
+
+    plt.figure()
+    for ss in samples8:
+        plt.plot(times, ss[:,0], 'b.')
+        plt.plot(times, ss[:,1], 'g.')
+
+
     plt.show()
 
 def lvsym(x, r, k, p=None):
@@ -110,8 +127,10 @@ def test_random_time_intervention():
     params = [r, k, alpha, sigma, gamma]
     t_max = 8.
 
-    init_x = []
-    for ii in range(100):
-        init_x.append(np.abs(np.random.normal(5., 0.5, 2)))
+#    init_x = []
+#    for ii in range(100):
+#        init_x.append(np.abs(np.random.normal(5., 0.5, 2)))
+    init_x = np.array([5., 4.])
     data = random_time_intervention(init_x, params, t_max, lvsym)
     print(data) 
+